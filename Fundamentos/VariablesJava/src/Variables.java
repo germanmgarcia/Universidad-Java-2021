e16fd14db94415a745f9c2d67e0a619fa961939e
Germán <gmgarcia@ucundinamarca.edu.co>
2021-09-03 14:30:15 -0500

    Ejercicio: Reglas para definir una variable en Java

--- Fundamentos/VariablesJava/src/Variables.java
+++ Fundamentos/VariablesJava/src/Variables.java
@@ -9,7 +9,7 @@
  * @author Germán
  */
 public class Variables {
-    
+
     public static void main(String[] args) {
         //Definimos la variable
         int miVariableEntera = 10;
@@ -17,18 +17,25 @@ public class Variables {
         //Modificamos el valor de la variable
         miVariableEntera = 5;
         System.out.println(miVariableEntera);
-        
+
         String miVariableCadena = "Saludos";
         System.out.println(miVariableCadena);
-        
+
         miVariableCadena = "Adiós";
         System.out.println(miVariableCadena);
 
         //var - Inferencia a tipos en Java
         var miVariableEntera2 = 15;
         System.out.println(miVariableEntera2);
-        
+
         var miVariableCadena2 = "Nueva Cadena";
         System.out.println("miVariableCadena2 = " + miVariableCadena2);
+
+        //Valores permitidos en el nombre de variables
+        var miVariable = 1;
+        var _miVariable = 2;
+        var $miVariable = 10;
+       //var áVariable = 10; No se recomienta utilizar
+       //var #miVariable = 2; No se permite utilizar caracteres especiales
     }
 }

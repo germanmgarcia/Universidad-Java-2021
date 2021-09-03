24c66ad30c82ef3952f107b084b91c0b4950ca51
Germán <gmgarcia@ucundinamarca.edu.co>
2021-09-03 11:56:56 -0500

    Tipos Enteros y Tipo String en Java

--- Fundamentos/VariablesJava/src/Variables.java
+++ Fundamentos/VariablesJava/src/Variables.java
@@ -11,7 +11,17 @@
 public class Variables {
 
     public static void main(String[] args) {
+        //Definimos la variable
         int miVariableEntera = 10;
         System.out.println(miVariableEntera);
+        //Modificamos el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+
+        miVariableCadena = "Adiós";
+        System.out.println(miVariableCadena);
     }
 }

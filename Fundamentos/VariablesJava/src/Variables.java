1236042411962d49a2834136721c068f89d7a448
Germán <gmgarcia@ucundinamarca.edu.co>
2021-09-03 12:02:04 -0500

    Inferencia de Tipos en Java

--- Fundamentos/VariablesJava/src/Variables.java
+++ Fundamentos/VariablesJava/src/Variables.java
@@ -9,7 +9,7 @@
  * @author Germán
  */
 public class Variables {
-
+    
     public static void main(String[] args) {
         //Definimos la variable
         int miVariableEntera = 10;
@@ -17,11 +17,18 @@ public class Variables {
         //Modificamos el valor de la variable
         miVariableEntera = 5;
         System.out.println(miVariableEntera);
-
+        
         String miVariableCadena = "Saludos";
         System.out.println(miVariableCadena);
-
+        
         miVariableCadena = "Adiós";
         System.out.println(miVariableCadena);
+
+        //var - Inferencia a tipos en Java
+        var miVariableEntera2 = 15;
+        System.out.println(miVariableEntera2);
+        
+        var miVariableCadena2 = "Nueva Cadena";
+        System.out.println("miVariableCadena2 = " + miVariableCadena2);
     }
 }

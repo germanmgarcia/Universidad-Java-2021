77e86b225517f6506939c33641078810214c6c05
Germán <gmgarcia@ucundinamarca.edu.co>
2021-09-06 10:39:37 -0500

    Tipos Primitivos en Java

---
+++ Fundamentos/TiposPrimitivos/src/TiposPrimitivos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Germán
+ */
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        /*
+            Tipos primitivos enteros: byte, short,int, long
+         */
+
+        byte numeroByte = (byte) 129;
+        System.out.println("numeroByte = " + numeroByte);
+        System.out.println("Valor mínimo byte: " + Byte.MIN_VALUE);
+        System.out.println("Valor maximo bye: " + Byte.MAX_VALUE);
+    }
+}

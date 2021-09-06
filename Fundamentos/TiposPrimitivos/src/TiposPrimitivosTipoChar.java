dcd8431db4f8160734ca1e2b4e39b182f761dcac
Germán <gmgarcia@ucundinamarca.edu.co>
2021-09-06 12:15:35 -0500

    Ejercicio: Tipos Primitivos tipo char

---
+++ Fundamentos/TiposPrimitivos/src/TiposPrimitivosTipoChar.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Germán
+ */
+public class TiposPrimitivosTipoChar {
+
+    public static void main(String[] args) {
+        char miCaracter = 'a';
+        System.out.println("args = " + miCaracter);
+
+        char varChar = '\u0021';
+        System.out.println("varChar = " + varChar);
+
+        char varCharDecimal = 33;
+        System.out.println("varCharDecimal = " + varCharDecimal);
+
+        char varCharSimbolo = '!';
+        System.out.println("varCharSimbolo = " + varCharSimbolo);
+
+        var varChar1 = '\u0021';
+        System.out.println("varChar1 = " + varChar1);
+
+        var varCharDecimal2 = 33;
+        System.out.println("varCharDecimal2 = " + varCharDecimal2);
+
+        var varCharSimbolo3 = '!';
+        System.out.println("varCharSimbolo3 = " + varCharSimbolo3);
+        
+        int variableEnteraSimbolo = '!';
+        System.out.println("variableEnteraSimbolo = " + variableEnteraSimbolo);
+        
+        int letra = 'A';
+        System.out.println("letra = " + letra);
+
+    }
+}

00a26e1adf266d1bea4e1009f41301b373643b5e
Germán <gmgarcia@ucundinamarca.edu.co>
2021-09-06 12:08:47 -0500

    Ejercicio: Inferencia de Tipos con Var y tipos Primitivos

---
+++ Fundamentos/TiposPrimitivos/src/TiposConVarYTiposPrimitivos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Germán
+ */
+public class TiposConVarYTiposPrimitivos {
+
+    public static void main(String[] args) {
+        var numeroEntero = 10;
+        System.out.println("numeroEntero = " + numeroEntero);
+
+        var numeroDouble = 10.0;
+        System.out.println("numeroEntero = " + numeroEntero);
+
+        var numeroFloat = 10.0F;
+        System.out.println("numeroFloat = " + numeroFloat);
+    }
+}

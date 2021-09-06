852de57165dd9175e4d4fd71d34a18dbb40d8071
Germán <gmgarcia@ucundinamarca.edu.co>
2021-09-06 10:44:51 -0500

    Tipos Numéricos Enteros

--- Fundamentos/TiposPrimitivos/src/TiposPrimitivos.java
+++ Fundamentos/TiposPrimitivos/src/TiposPrimitivos.java
@@ -19,5 +19,20 @@ public class TiposPrimitivos {
         System.out.println("numeroByte = " + numeroByte);
         System.out.println("Valor mínimo byte: " + Byte.MIN_VALUE);
         System.out.println("Valor maximo bye: " + Byte.MAX_VALUE);
+
+        short numeroShort = (short) 32768;
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("Valor mínimo short: " + Short.MIN_VALUE);
+        System.out.println("Valor maximo short: " + Short.MAX_VALUE);
+
+        int numeroInt = (int) 2147836488L;
+        System.out.println("numeroInt = " + numeroInt);
+        System.out.println("Valor mínimo int: " + Integer.MIN_VALUE);
+        System.out.println("Valor maximo int: " + Integer.MAX_VALUE);
+
+        long numeroLog = 9223372036854775807L;
+        System.out.println("numeroLog = " + numeroLog);
+        System.out.println("Valor mínimo log: " + Long.MIN_VALUE);
+        System.out.println("Valor maximo log: " + Long.MAX_VALUE);
     }
 }

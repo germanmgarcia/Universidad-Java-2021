fd326dc31006caadac74d0ae3e35d0ba4de5aa97
Germán <gmgarcia@ucundinamarca.edu.co>
2021-09-23 21:39:05 -0500

    Ejercicio: Operadores Relacionales

--- Fundamentos/OperadoresAritmeticos/src/OperadoresIgualdadRelacionales.java
+++ Fundamentos/OperadoresAritmeticos/src/OperadoresIgualdadRelacionales.java
@@ -9,23 +9,41 @@
  * @author Germán
  */
 public class OperadoresIgualdadRelacionales {
-    
+
     public static void main(String[] args) {
         var a = 3;
         var b = 2;
-        
+
         var c = (a == b);
         System.out.println("c = " + c);
-        
+
         var d = a != b;
         System.out.println("d = " + d);
-        
+
         var cadena = "Hola";
         var cadena2 = "Adios";
         var e = cadena == cadena2;//comprar referencias de objetos
         System.out.println("e = " + e);
-        
+
         var f = cadena.equals(cadena2);//comparamos contenido de cadenas
         System.out.println("f = " + f);
+
+        //Operadores Relacionales
+        var g = a >= b;//mayor que > o el mayor o igual >=
+        System.out.println("g = " + g);
+
+        if (a % 2 == 0) {
+            System.out.println("Es numero par");
+        } else {
+            System.out.println("Es numero impar");
+        }
+
+        var edad = 30;
+        var adulto = 18;
+        if (edad >= adulto) {
+            System.out.println("Es un adulto");
+        } else {
+            System.out.println("Es menor de edad");
+        }
     }
 }

7c6ceb709b1ac7c754c7f0e8e542897696703baf
Germán <gmgarcia@ucundinamarca.edu.co>
2021-09-25 22:58:13 -0500

    Sentencia de control if - else en Java

---
+++ Fundamentos/SentenciasDeControl/src/SentenciaDeControlIfElse.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Germán
+ */
+public class SentenciaDeControlIfElse {
+
+    public static void main(String[] args) {
+        var condicion = true;
+
+        if (condicion == true) {
+            System.out.println("Condición verdadera");
+        } else {
+            System.out.println("Condición falso");
+        }
+    }
+}

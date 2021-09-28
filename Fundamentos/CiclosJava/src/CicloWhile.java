bb92eca4e31d43ff099e613bbb448754874de47b
Germán <gmgarcia@ucundinamarca.edu.co>
2021-09-27 21:54:27 -0500

    Ejercicio Ciclo While

---
+++ Fundamentos/CiclosJava/src/CicloWhile.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Germán
+ */
+public class CicloWhile {
+
+    public static void main(String[] args) {
+        var contador = 0;
+        while (contador < 3) {
+            System.out.println("contador = " + contador);
+            contador++;
+        }
+    }
+}

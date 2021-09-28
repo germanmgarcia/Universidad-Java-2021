a0329ff9d186cdaf17758ebc2e19b8b25e58ba95
Germán <gmgarcia@ucundinamarca.edu.co>
2021-09-27 22:04:34 -0500

    Ejercicio: Palabras Break y Continue en Java

--- Fundamentos/CiclosJava/src/CicloFor.java
+++ Fundamentos/CiclosJava/src/CicloFor.java
@@ -11,7 +11,16 @@
 public class CicloFor {
 
     public static void main(String[] args) {
+//        for (var contador = 0; contador < 3; contador++) {
+//            if (contador % 2 == 0) {
+//                System.out.println("contador = " + contador);
+//                break;
+//            }
+//        }
         for (var contador = 0; contador < 3; contador++) {
+            if (contador % 2 != 0) {
+                continue;//Ir a la siguiente iteración
+            }
             System.out.println("contador = " + contador);
         }
     }

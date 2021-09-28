bb691e26639396cae088168d55a322025bcf7635
Germán <gmgarcia@ucundinamarca.edu.co>
2021-09-27 21:22:47 -0500

    Sentencia de control switch en Java

---
+++ Fundamentos/SentenciasDeControl/src/ControlSwitch.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Germán
+ */
+public class ControlSwitch {
+
+    public static void main(String[] args) {
+        var numero = -3;
+        var numeroTexto = "Valor desconocido";
+        switch (numero) {
+            case 1:
+                numeroTexto = "Numero uno";
+                break;
+            case 2:
+                numeroTexto = "Numero dos";
+                break;
+            case 3:
+                numeroTexto = "Numero tres";
+                break;
+            case 4:
+                numeroTexto = "Numero cuatro";
+                break;
+            default:
+                numeroTexto = "Caso no encontrado";
+        }
+        System.out.println("numeroTexto = " + numeroTexto);
+    }
+}

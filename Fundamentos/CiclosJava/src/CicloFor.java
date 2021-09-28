a0b3a4bb00e36160407f374ad453c6f426c037e8
Germán <gmgarcia@ucundinamarca.edu.co>
2021-09-27 22:07:18 -0500

    Ejercicio: Uso de Etiquetas (Labels) en Java

--- Fundamentos/CiclosJava/src/CicloFor.java
+++ Fundamentos/CiclosJava/src/CicloFor.java
@@ -11,17 +11,19 @@
 public class CicloFor {
 
     public static void main(String[] args) {
-//        for (var contador = 0; contador < 3; contador++) {
+        inicio:
+        for (var contador = 0; contador < 3; contador++) {
+            if (contador % 2 != 0) {
+                continue inicio;//Ir a la linea de codigo de la etiqueta
+            }
+            System.out.println("contador = " + contador);
+        }
+
+        //        for (var contador = 0; contador < 3; contador++) {
 //            if (contador % 2 == 0) {
 //                System.out.println("contador = " + contador);
 //                break;
 //            }
 //        }
-        for (var contador = 0; contador < 3; contador++) {
-            if (contador % 2 != 0) {
-                continue;//Ir a la siguiente iteración
-            }
-            System.out.println("contador = " + contador);
-        }
     }
 }

4e36a1c3bd31edf6b0a065a08b006b3db88ce606
Germán <gmgarcia@ucundinamarca.edu.co>
2021-10-15 12:48:11 -0500

    Objetos en Java - continuación

--- Fundamentos/ClasesEnJava/src/clases/PruebaPersona.java
+++ Fundamentos/ClasesEnJava/src/clases/PruebaPersona.java
@@ -3,10 +3,17 @@ package clases;
 public class PruebaPersona {
 
     public static void main(String[] args) {
-        Persona persona1;
-        persona1 = new Persona();
+        Persona persona1 = new Persona();
         persona1.nombre = "Germán";
         persona1.apellido = "García";
         persona1.desplegarInformacion();
+
+        Persona persona2 = new Persona();
+        System.out.println("persona1 = " + persona1);
+        System.out.println("persona2 = " + persona2);
+
+        persona2.nombre = "Karen";
+        persona2.apellido = "Barrera";
+        persona2.desplegarInformacion();
     }
 }

bfc26ff41c760f268f2632970da5d25f03588b5e
Germán <gmgarcia@ucundinamarca.edu.co>
2021-10-15 12:42:36 -0500

    Objetos en Java

---
+++ Fundamentos/ClasesEnJava/src/clases/PruebaPersona.java
@@ -0,0 +1,12 @@
+package clases;
+
+public class PruebaPersona {
+
+    public static void main(String[] args) {
+        Persona persona1;
+        persona1 = new Persona();
+        persona1.nombre = "Germán";
+        persona1.apellido = "García";
+        persona1.desplegarInformacion();
+    }
+}
